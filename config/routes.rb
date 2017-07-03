c6c01ee1e549eb1dbace7360e82bc68163b7233b
reshma-r-shenoy <reshma@redpanthers.co>
2017-07-03 11:48:38 +0530

    add insurance pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :homes do
     collection do
       get :co_reg
+      get :life
+      get :health
+      get :auto
+      get :home
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
